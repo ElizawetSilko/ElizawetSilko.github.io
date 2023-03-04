a8f09b8ab69e88b042f99b7e553bb9e324c3684a
ElizawetSilko <123663994+ElizawetSilko@users.noreply.github.com>
2023-03-04 18:54:01 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+
+var button = document.querySelector('button');
+
+button.addEventListener('click', function() {
+	alert('«Чем умнее человек, тем легче он признает себя дураком». Альберт Эйнштейн')
+})
